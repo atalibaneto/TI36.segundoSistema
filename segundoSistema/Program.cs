7a0d5b4510e29c7c71335c9f74e16718d271d9ac
atalibaneto <atalibaneto@gmail.com>
2025-02-11 09:46:59 -0300

    Adicionar arquivos de projeto.

---
+++ segundoSistema/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundoSistema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Operadores Lógicos
+            && - retorna true se ambas as instruções forem verdadeiras
+            || - retorna true se uma das instruções for verdadeira
+            !  - reverte o resulatado, retorna falso se o resultado for verdadeiro
+             */
+
+        }
+    }
+}
